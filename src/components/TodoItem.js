d17f442d51154a0b2be9b5f33d1d9b6e18eaf76c
alexandreKANE <alexandre.kane.34750@gmail.com>
2017-12-19 14:36:38 +0100

    DaPush x

---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './todoItem.css';
+
+export default class todoItem extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+  render(){
+    return(
+      <div className="todoWrapper">
+        <button className="removeTodo" onClick={(e)=> this.removeTodo(this.props.id)}>x</button>{this.props.todo.text}
+      </div>
+    )
+  }
+}
